c4a062178ea0adc2c146018588b15be2621c1dfa
Jayantajit Gogoi <jayanta.gogoi525@gmail.com>
2020-07-01 04:16:19 +0000

    circleci: adjust to checkout repo
    - uses user's own repo for config.seed

--- scripts/adjust-feeds.sh
+++ scripts/adjust-feeds.sh
@@ -1,12 +1,14 @@
 # irqbalance
 sed -i 's/0/1/g' feeds/packages/utils/irqbalance/files/irqbalance.config
 #luci architecture
-sed -i "s,boardinfo.system,'ARMv8',g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+sed -i "s,boardinfo.system,'ARMv8 64bit Processor',g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 # Maximum connection
 sed -i 's/16384/65536/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 #irq_optimize
 mkdir package/base-files/files/usr/bin
-wget https://raw.githubusercontent.com/jayanta525/openwrt-nanopi-r2s/circleci/files/irq_optimize.sh -O package/base-files/files/usr/bin/irq_optimize.sh
+rm package/base-files/files/usr/bin/irq_optimize.sh
+cp ../github/files/irq_optimize.sh package/base-files/files/usr/bin/irq_optimize.sh
 chmod +x package/base-files/files/usr/bin/irq_optimize.sh
-wget https://raw.githubusercontent.com/jayanta525/openwrt-nanopi-r2s/circleci/files/irq_optimize -O package/base-files/files/etc/init.d/irq_optimize
+rm package/base-files/files/etc/init.d/irq_optimize
+cp ../github/files/irq_optimize package/base-files/files/etc/init.d/irq_optimize
 chmod +x package/base-files/files/etc/init.d/irq_optimize
